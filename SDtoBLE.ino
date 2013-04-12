96dcc0924b175abe88db1af73c940127d67978c0
gregpaton08 <gregpaton08@gmail.com>
2013-04-11 22:52:40 -0400

    [mod] BLE working with SD shield attached!

--- SDtoBLE.ino
+++ SDtoBLE.ino
@@ -3,7 +3,7 @@
 #include "ble.h"
 
 #define SD_SS 4
-#define BLE_SS 53
+#define MEGA_SS 53
 #define BLE_REQN 9
 
 File OBDLog;
@@ -12,17 +12,44 @@ void setup() {
   // Set serial rate
   Serial.begin(38400);
   
+  pinMode(MEGA_SS, OUTPUT);
+  
   // Setup SD card
   pinMode(SD_SS, OUTPUT);
   digitalWrite(SD_SS, HIGH);
   
-  // Set up Serial Peripheral Interface
-  SPI.setDataMode(SPI_MODE0);
+  // Set up Serial Peripheral Interface for BLE
+  /*SPI.setDataMode(SPI_MODE0);
   SPI.setBitOrder(LSBFIRST);
   SPI.setClockDivider(SPI_CLOCK_DIV16);
   SPI.begin();
   
   // Start BLE
+  ble_begin();
+  
+  
+  // Set up Serial Peripheral Interface for BLE
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(MSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV4);
+  SPI.begin();
+  
+  digitalWrite(SD_SS, LOW);
+  
+  SD.begin();
+  
+  OBDLog = SD.open("hi.txt", FILE_WRITE);
+  if (OBDLog)
+    Serial.println("YES");
+    
+  digitalWrite(SD_SS, HIGH);*/
+  
+  // Set up Serial Peripheral Interface for BLE
+  SPI.setDataMode(SPI_MODE0);
+  SPI.setBitOrder(LSBFIRST);
+  SPI.setClockDivider(SPI_CLOCK_DIV16);
+  SPI.begin();
+  
   ble_begin();
 }
 
